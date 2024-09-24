d767dcb40d05ed40f8d951fdf5bf78cf6b3f6432
Dmytro Kopaihora <47076154+Dimonjik777@users.noreply.github.com>
2024-09-24 12:52:31 +0300

    Add some js feature
    In commit was added modal window for images, accordion and choosing creators

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,94 @@
+// accordion
+
+// Take all faqTitles
+let faqHeaders = document.querySelectorAll(".faq__item__header");
+
+faqHeaders.forEach((faqHeader) => {
+
+  faqHeader.addEventListener("click", () => {
+
+    // Take parentElement
+    const faqItem = faqHeader.parentElement;
+
+    // Take arrow img and main text
+    const accordionArrow = faqItem.querySelector("img");
+    const accordionContent = faqItem.querySelector(".faq__item__content");
+    
+    accordionContent.classList.toggle("active");
+
+    if(accordionContent.classList.contains("active")){
+      accordionArrow.style.transform = `rotate(-180deg)`;
+      accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
+    }
+    else{
+      accordionArrow.style.transform = `rotate(0)`;
+      accordionContent.style.maxHeight = 0;
+    }
+
+  });
+
+});
+
+
+// Declare vars for modal window
+
+let modalWindow = null;
+let modalImage = null
+
+// Fade in modal window on current image
+function modalImg(id){
+
+  // Create DOM elements
+  modalWindow = document.createElement("div");
+  modalImage = document.createElement("img");
+
+  // Add class
+  modalWindow.classList.add("modal__window");
+
+  // Find current image
+  modalImage.setAttribute("src", `src/img/nft ${id}.png`);
+
+  // Add image to container
+  modalWindow.append(modalImage);
+
+  // Render modal image
+  document.body.prepend(modalWindow);
+
+  // Fade in bg in modal
+  setTimeout(() => {
+    modalWindow.style.opacity = 1;
+  }, 100);
+
+  // Fade in image
+  setTimeout(() => {
+    modalImage.style.opacity = 1;
+    modalImage.style.transform = "translateY(0)";
+  }, 500);
+}
+
+document.addEventListener("click", event => {
+
+  if(event.target.classList.contains("modal__window")){
+    setTimeout(() => {
+      modalImage.style.opacity = 0;
+      modalImage.style.transform = "translateY(-60px)";
+    }, 100);
+
+    setTimeout(() => {
+      modalWindow.style.opacity = 0;
+    }, 300);
+
+    setTimeout(() => {
+      modalWindow.remove();
+      modalWindow = null;
+      modalImage = null;
+    }, 500);
+  }
+
+});
+
+
+
 function chooseCreator(id) {
 
   // Delete active style in button
@@ -42,11 +133,9 @@ function chooseCreator(id) {
     creatorText.style.opacity = 1;
     creatorImg.style.opacity = 1;
 
-    //Add active state style
-    document.querySelectorAll(".creators__btns button")[id].classList.add("creator__active");
-
-
   }, 200);
 
+  //Add active state style
+  document.querySelectorAll(".creators__btns button")[id].classList.add("creator__active");
 
 }
